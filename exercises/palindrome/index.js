3f55713ae50d113e3b44e3f4773ee9998f219a41
patelsaheb1219 <kaumik1219@gmail.com>
2021-09-24 22:23:33 -0400

    Problem: Check Palindrome String

---
+++ exercises/palindrome/index.js
@@ -0,0 +1,23 @@
+// --- Directions
+// Given a string, return true if the string is a palindrome
+// or false if it is not.  Palindromes are strings that
+// form the same word if it is reversed. *Do* include spaces
+// and punctuation in determining if the string is a palindrome.
+// --- Examples:
+//   palindrome("abba") === true
+//   palindrome("abcdefg") === false
+
+function palindrome(str) {
+  /**
+   * Solve using reduce function
+      const reverseStr = str.split("").reduce((rev, char) => char + rev, "");
+  return str === reverseStr;
+   */
+
+  // Solve using every function
+  return str.split("").every((char, i) => {
+    return char === str[str.length - 1 - i];
+  })
+}
+
+module.exports = palindrome;
