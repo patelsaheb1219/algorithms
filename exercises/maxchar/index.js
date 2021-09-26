3a580a4de037dd047a0a305a506e90dfdccf1e82
patelsaheb1219 <kaumik1219@gmail.com>
2021-09-25 22:11:18 -0400

    Problem: Find maximum character used in given string

---
+++ exercises/maxchar/index.js
@@ -0,0 +1,29 @@
+// --- Directions
+// Given a string, return the character that is most
+// commonly used in the string.
+// --- Examples
+// maxChar("abcccccccd") === "c"
+// maxChar("apple 1231111") === "1"
+
+function maxChar(str) {
+  const characters = {};
+  let max = 0;
+  let maxChar = '';
+
+  for (let char of str) {
+    if (characters[char]) {
+      characters[char]++;
+    } else {
+      characters[char] = 1;
+    }
+  }
+  for (let char in characters) {
+    if (characters[char] > max) {
+      max = characters[char];
+      maxChar = char;
+    }
+  }
+  return maxChar;
+};
+
+module.exports = maxChar;
