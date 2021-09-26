c89d6fb355aeea19293ff7974d9989d73a139c95
patelsaheb1219 <kaumik1219@gmail.com>
2021-09-25 23:47:49 -0400

    Problem: Chunked Array

---
+++ exercises/chunk/index.js
@@ -0,0 +1,34 @@
+// --- Directions
+// Given an array and chunk size, divide the array into many subarrays
+// where each subarray is of length size
+// --- Examples
+// chunk([1, 2, 3, 4], 2) --> [[ 1, 2], [3, 4]]
+// chunk([1, 2, 3, 4, 5], 2) --> [[ 1, 2], [3, 4], [5]]
+// chunk([1, 2, 3, 4, 5, 6, 7, 8], 3) --> [[ 1, 2, 3], [4, 5, 6], [7, 8]]
+// chunk([1, 2, 3, 4, 5], 4) --> [[ 1, 2, 3, 4], [5]]
+// chunk([1, 2, 3, 4, 5], 10) --> [[ 1, 2, 3, 4, 5]]
+
+const chunk = (array, size) => {
+  /**
+    let chunked = [];
+    for (let i of array) {
+      const lastElement = chunked[chuncked.length - 1];
+      if (!lastElement || lastElement.length === size) {
+        chunked.push([i]);
+      } else {
+        lastElement.push(i);
+      }
+    }
+    return chuncked;
+  */
+  const chunked = [];
+  let index = 0;
+
+  while (index < array.length) {
+    chunked.push(array.slice(index, index + size));
+    index += size;
+  }
+  return chunked;
+}
+
+module.exports = chunk;
