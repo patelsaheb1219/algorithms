d894d9c3c1088c678ce328d7a8023df78fc03848
patelsaheb1219 <kaumik1219@gmail.com>
2021-10-09 17:32:54 -0400

    Problem: Write steps using # and space using recursion method

--- exercises/steps/index.js
+++ exercises/steps/index.js
@@ -17,7 +17,9 @@
 //       '### '
 //       '####'
 
-const steps = (n) => {
+const steps = (n, row = 0, stair = "") => {
+  /**
+   *  Solution 1
   for (let row = 0; row < n; row++) {
     let stair = "";
     for (let column = 0; column < n; column++) {
@@ -30,6 +32,23 @@ const steps = (n) => {
     }
     console.log(stair);
   }
+
+  */
+
+  /**
+   * Solution 2
+   */
+  if (n === row) {
+    return;
+  }
+
+  if (n === stair.length) {
+    console.log(stair);
+    return steps(n, row + 1);
+  }
+
+  const add = stair.length <= row ? "#" : " "
+  steps(n, row, stair + add);
 };
 
 module.exports = steps;
