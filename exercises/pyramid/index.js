cdb09677e3e6d623c6f19e0c21555afff2431349
patelsaheb1219 <kaumik1219@gmail.com>
2021-10-10 18:35:40 -0400

    Problem: Pyramid using recursive solution

--- exercises/pyramid/index.js
+++ exercises/pyramid/index.js
@@ -14,19 +14,43 @@
 //       ' ### '
 //       '#####'
 
-function pyramid(n) {
-  const midpoint = Math.floor((2 * n - 1) / 2);
-  for (let row = 0; row < n; row++) {
-    let level = '';
-    for (let column = 0; column < (n * 2)-1; column++) {
-      if (midpoint - row <= column && midpoint + row >= column) {
-        level += '#';
-      } else {
-        level += ' ';
-      }
-    }
+function pyramid(n, row = 0, level = '') {
+  /**
+   * Solution 1
+   */
+  // const midpoint = Math.floor((2 * n - 1) / 2);
+  // for (let row = 0; row < n; row++) {
+  //   let level = '';
+  //   for (let column = 0; column < (n * 2)-1; column++) {
+  //     if (midpoint - row <= column && midpoint + row >= column) {
+  //       level += '#';
+  //     } else {
+  //       level += ' ';
+  //     }
+  //   }
+  //   console.log(level);
+  // }
+
+  /**
+   * Solution 2
+   */
+  if (n === row) {
+    return;
+  } 
+
+  if (level.length === 2 * n - 1) {
     console.log(level);
+    return pyramid(n, row + 1);
   }
+
+  const midpoint = Math.floor((2*n - 1) / 2);
+  let add;
+  if (midpoint - row <= level.length && midpoint + row >= level.length) {
+    add = '#';
+  } else {
+    add = ' ';
+  }
+  pyramid(n, row, level + add);
 }
 
 module.exports = pyramid;
