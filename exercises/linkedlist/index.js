f7f149e8b948e4fc5454c32ba7ed9d689d855067
patelsaheb1219 <kaumik1219@gmail.com>
2022-01-09 20:01:12 -0500

    Problem: LinkedList, Created removeAt function in LinkedList Class

--- exercises/linkedlist/index.js
+++ exercises/linkedlist/index.js
@@ -106,6 +106,22 @@ class LinkedList {
     }
     return null;
   }
+
+  removeAt = (index) => {
+    if (!this.head) {
+      return;
+    }
+
+    if (index === 0) {
+      this.head = this.head.next;
+    }
+
+    const previous = this.getAt(index - 1);
+    if (!previous || !previous.next) {
+      return;
+    }
+    previous.next = previous.next.next;
+  }
 }
 
 module.exports = { Node, LinkedList };
