ac517e2117bebb3cf91691f876ba5b8b45485c23
patelsaheb1219 <kaumik1219@gmail.com>
2022-01-11 11:26:34 -0500

    Problem: Tree, Created Node class and implemented remove function within it

--- exercises/tree/index.js
+++ exercises/tree/index.js
@@ -20,8 +20,10 @@ class Node {
     this.children.push(new Node(data));
   }
 
-  remove = () => {
-    
+  remove = (data) => {
+    this.children = this.children.filter(child => {
+      return child.data !== data;
+    })
   }
 }
 
